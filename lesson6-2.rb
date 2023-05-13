210cdb00a0b3f48c27abe5c3a8142f5953fab40b
ToriMun <132326048+torimun@users.noreply.github.com>
2023-05-13 17:58:49 +0000

    [Finish]Rubyを学ぼうより　lesson6-2演習問題初期登録

---
+++ lesson6-2.rb
@@ -0,0 +1,14 @@
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+input = gets.to_i
+
+i = 1
+while i < input do
+  puts "計算結果を出力します。"
+
+puts "[i]回目の計算"
+puts "2つの値を入力して下さい"
+a=gets
+b=gets
+
+puts "計算結果を出力します。"
