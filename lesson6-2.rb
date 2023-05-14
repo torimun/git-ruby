d1ae22998cb2f71a185f69285d10d9022c73ff38
ToriMun <132326048+torimun@users.noreply.github.com>
2023-05-14 06:23:20 +0000

    [Finish]Rubyを学ぼうより　lesson6-2演習問題完了

--- lesson6-2.rb
+++ lesson6-2.rb
@@ -3,12 +3,23 @@ puts "何回繰り返しますか？"
 input = gets.to_i
 
 i = 1
-while i < input do
-  puts "計算結果を出力します。"
+while i <= input do
+  puts "[i]回目の計算"
+  puts "2つの値を入力して下さい"
 
-puts "[i]回目の計算"
-puts "2つの値を入力して下さい"
-a=gets
-b=gets
+  a=gets.to_i
+  b=gets.to_i
 
-puts "計算結果を出力します。"
+  puts"a=#{a}"
+  puts"a=#{b}"
+
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a+b}"
+  puts "#{a}-#{b}=#{a-b}"
+  puts "#{a}*#{b}=#{a*b}"
+  puts "#{a}/#{b}=#{a/b}"
+
+  i +=1
+end
+
+puts "計算を終了します"
